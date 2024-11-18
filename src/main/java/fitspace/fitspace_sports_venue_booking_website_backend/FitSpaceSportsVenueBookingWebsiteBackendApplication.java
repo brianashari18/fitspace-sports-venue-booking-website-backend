40e2eea4b6de370d3646e1b0dd866f43237d4c1b
Brian Anashari <brianashari18@gmail.com>
2024-11-18 21:21:14 +0700

    add api spec

---
+++ src/main/java/fitspace/fitspace_sports_venue_booking_website_backend/FitSpaceSportsVenueBookingWebsiteBackendApplication.java
@@ -0,0 +1,13 @@
+package fitspace.fitspace_sports_venue_booking_website_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitSpaceSportsVenueBookingWebsiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitSpaceSportsVenueBookingWebsiteBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fitspace/fitspace_sports_venue_booking_website_backend/FitSpaceSportsVenueBookingWebsiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fitspace.fitspace_sports_venue_booking_website_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitSpaceSportsVenueBookingWebsiteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
